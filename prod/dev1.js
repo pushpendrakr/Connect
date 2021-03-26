027bb038d5d292ff84607ffb43ae359d20ed6f3a
pushpendrakr <52634462+pushpendrakr@users.noreply.github.com>
2021-03-27 03:03:12 +0530

    code

--- prod/dev1.js
+++ prod/dev1.js
@@ -1,3 +1,3 @@
 module.exports={
-    MONGOURI:"//pushpendra:<VaWLq2PDnELj2Vj>@cluster0-shard-00-00.mbmqo.mongodb.net:27017,cluster0-shard-00-01.mbmqo.mongodb.net:27017,cluster0-shard-00-02.mbmqo.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-sivjak-shard-0&authSource=admin&retryWrites=true&w=majority"
+    MONGOURI:"mongodb://pushpendra:VaWLq2PDnELj2Vj@cluster0-shard-00-00.mbmqo.mongodb.net:27017,cluster0-shard-00-01.mbmqo.mongodb.net:27017,cluster0-shard-00-02.mbmqo.mongodb.net:27017/mynewdb1?ssl=true&replicaSet=atlas-sivjak-shard-0&authSource=admin&retryWrites=true&w=majority"
 }
