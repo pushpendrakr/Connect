983b05ffc18695ccd0f0175f47b62ab011307a75
pushpendrakr <52634462+pushpendrakr@users.noreply.github.com>
2021-03-26 23:47:17 +0530

    make it better

--- prod/keys.js
+++ prod/keys.js
@@ -1,1 +1,6 @@
-module.exports = require('./prod1')
+if(process.env.NODE_ENV==='production'){
+module.exports = require('./prod1.js')
+}
+else {
+    module.exports=require('./dev1.js')
+}
--- prod/prod1.js
+++ prod/prod1.js
@@ -1,3 +1,3 @@
 module.exports={
-    MONGOURI:process.env.MONGOURI
+    MONGOURI:process.env.MONGOURI,
 }
